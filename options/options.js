283530466c54be0a1a3fe07a6774a1de8ed13ef7
Crowdbotics <team@crowdbotics.com>
2023-06-06 22:50:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-limit-41313",
+  url: "https://super-limit-41313.botics.co",
+  api: "https://super-limit-41313.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
